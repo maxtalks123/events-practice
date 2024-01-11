b6761e076030c5f43a3d29595b5b6beadcb0cea8
Max Emerson <maxemerson2013@gmail.com>
2024-01-11 19:31:48 +0000

    experimented with different functions and managed to do one

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("hello, events people!");
+const button = document.querySelector("button");
+console.log(button.textContent);
+
+//function
+function spooky() {
+  document.body.style.background = "tomato";
+  console.log("Congratulations, you pressed the button");
+}
+
+const welcome = prompt("Whats your name?");
+alert("hello", prompt);
+
+function funtimes() {}
+
+button.addEventListener("mouseover", moveTo(0, 0));
+button.addEventListener("click", spooky);
